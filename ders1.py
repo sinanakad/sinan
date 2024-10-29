d01080b4e03ea1f509480f7e5b77659116aee2c5
sinanakad <sinanakad@gmail.com>
2024-10-29 15:05:48 +0300

    1

--- ders1.py
+++ ders1.py
@@ -2,3 +2,6 @@ ad = "sinan"
 print("iyi günler",ad)
 print("iyi günler"+ad)
 print("iyi günler",ad)
+# virgül boşluk anlamına geliyor komutları yazarken + ise birleştirme anlamına gelyor
+# deneme notu
+
