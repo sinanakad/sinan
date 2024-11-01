7e02770426e21f9a26c365898bd21d9fa22f4dd2
sinanakad <sinanakad@gmail.com>
2024-11-01 22:38:04 +0300

    01.11.2024

--- ders2.py
+++ ders2.py
@@ -2,8 +2,8 @@ print("merhaba")
 ad = input("adınız?")
 print("adın güzelmiş", ad)
 dy= int(input ("kaç doğumlusun"))
-print("demek",2024-dy,"yaşındasın")
-
-
+print("demek",2024-dy,"yaşındasın",ad)
+ıl= input ("nerelisin")
+print(ıl,'çok severim')
 
 
